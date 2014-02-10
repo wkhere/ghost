9a76d457cea0fa0dafe9880508602dc81aa37831
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-02-10 14:10:51 +0100

    stuff to start w/ github api

--- lib/ghost.ex
+++ lib/ghost.ex
@@ -1,2 +1,10 @@
 defmodule Ghost do
+  use HTTPotion.Base
+
+  def process_url(url), do: "https://api.github.com/" <> url
+  def process_request_headers(hdrs), do: Dict.put(hdrs, "User-Agent", "ghost")
+
+  def process_response_body(body) do
+    body |> to_string |> :jsx.decode
+  end
 end
