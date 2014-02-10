64ee5905a6b10e049d437639a58fe97c41f4d27a
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-02-10 16:30:21 +0100

    no implicit slash

--- lib/ghost.ex
+++ lib/ghost.ex
@@ -1,7 +1,7 @@
 defmodule Ghost do
   use HTTPotion.Base
 
-  def process_url(url), do: "https://api.github.com/" <> url
+  def process_url(url), do: "https://api.github.com" <> url
   def process_request_headers(hdrs), do: Dict.put(hdrs, "User-Agent", "ghost")
 
   def process_response_body(body) do
